4f9957ad26806e05c2a598b44104b1dd41d7d5e0
Shawn Stovall <stovall.mail@gmail.com>
2015-05-21 20:33:34 -0400

    added chapter 3

---
+++ Chapter 3/chap3.rb
@@ -0,0 +1,24 @@
+myString = '...you can say taht again...'
+puts myString
+puts myString
+
+name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name + ' is a really long name!'
+
+var = 'just another ' + 'string'
+puts var
+
+var = 5 * (1+2)
+puts var
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
